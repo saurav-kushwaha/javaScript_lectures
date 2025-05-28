6f3c52a08a8ec93410d7e454c5ba1c602dfc7cc8
saurav-kushwaha <sauravkushwaha2219@gmail.com>
2025-05-28 16:56:50 +0530

    static keyword in js

---
+++ oop_and_classes_lecture41/static.js
@@ -0,0 +1,28 @@
+// static keyword in JavaScript is used to define static methods and properties in a class.
+// Static methods are called on the class itself, not on instances of the class.
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Username: ${this.username}`);
+    }
+
+    static createId(){
+        return `123`
+    }
+}
+
+const hitesh = new User("hitesh")
+// console.log(hitesh.createId())
+
+class Teacher extends User {
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com")
+console.log(iphone.createId());
